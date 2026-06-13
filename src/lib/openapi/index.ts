2676bf719c10c8230dbb7b4da4162b871872be09
proSamik <samikchoudhury15@gmail.com>
2026-06-13 18:14:56 +0530

    chore: update src/lib/openapi/index.ts

--- src/lib/openapi/index.ts
+++ src/lib/openapi/index.ts
@@ -1,7 +1,6 @@
-import path from 'node:path'
 import { createOpenAPI } from 'fumadocs-openapi/server'
 
 export const openapi = createOpenAPI({
-  input: [path.resolve('./content/docs/api-reference/openapi.yml')],
+  input: ['content/docs/api-reference/openapi.yml'],
   proxyUrl: '/api/proxy',
 })
