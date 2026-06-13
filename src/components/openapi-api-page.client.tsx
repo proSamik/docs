09b31e55803b8e634cf242905687301beeff88a8
proSamik <samikchoudhury15@gmail.com>
2026-06-13 18:30:27 +0530

    chore: strip style/className from OpenAPI client page props

--- src/components/openapi-api-page.client.tsx
+++ src/components/openapi-api-page.client.tsx
@@ -1,5 +1,14 @@
 'use client'
 
-import * as React from 'react'
+import type { ComponentProps } from 'react'
+import BaseOpenAPIClientPage from './api-page.client'
 
-export { default } from './api-page.client'
+type OpenAPIClientPageProps = ComponentProps<typeof BaseOpenAPIClientPage>
+
+export default function OpenAPIClientPage({
+  style: _style,
+  className: _className,
+  ...props
+}: OpenAPIClientPageProps & { style?: unknown; className?: unknown }) {
+  return <BaseOpenAPIClientPage {...(props as OpenAPIClientPageProps)} />
+}
